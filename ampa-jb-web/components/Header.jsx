60c46a4213e8afa63fef9471414e634cbfd868b9
kkrotDev <moyo.gsxr@gmail.com>
2023-03-12 12:58:44 +0100

    Avances en la creación del Header

--- ampa-jb-web/components/Header.jsx
+++ ampa-jb-web/components/Header.jsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Header() {
   return (
-    <header className=' bg-gray-100 p-6'>
-      <div className='border border-violet-900 rounded-3xl container grid grid-cols-1 m-auto lg:grid-cols-3 gap-1 p-2 lg:p-6'>
+    <header className=' bg-gray-100 p-2'>
+      <div className='container grid grid-cols-1 m-auto lg:grid-cols-3 gap-1 p-2'>
         <div className='p-2 flex justify-center lg:col-start-2 lg:col-end-3 lg:row-start-1'>
           <Image
             src='/assets/header/logo.png'
@@ -15,15 +16,21 @@ export default function Header() {
         <div className='text-blue-600 text-center text-3xl lg:col-start-1 lg:col-end-2 lg:row-start-1'>
           Header - 1
         </div>
-        <div className='mx-auto lg:col-star-3 lg:col-end-4 lg:row-start-1'>
-          <div className='p-2 lg:p-6 flex-row justify-around text-center font-sans lg:text-right'>
-            <h1 className='text-xl font-bold'>
-              Colegio "Jesuitinas Bilbao" Ikastetxea
+        <div className='lg:col-star-3 lg:col-end-4 lg:row-start-1 flex align-middle'>
+          <div className='p-2 lg:p-6 text-center font-sans lg:text-right m-auto '>
+            <h1 className='text-lg font-bold'>
+              AMPA "Jesuitinas Bilbao" Ikastetxea
             </h1>
-            <p className='text-naranja-900'>C/ Zabalbide 141, 48015 - Bilbao</p>
-            <p className='text-naranja-900'>Tlfo: 94 445 42 00 </p>
-            <p className='text-naranja-900'>
-              E-Mail: AMPA (Plataforma EDUCAMOS)
+            <p className='text-naranja-900 text-xs'>
+              C/ Zabalbide 141, 48015 - Bilbao
+            </p>
+            <p className='text-naranja-900 text-xs'>
+              Tlfo: 94 445 42 00 (Horario escolar)
+            </p>
+            <p className='text-naranja-900 text-xs hover:text-morado-900 hover:font-bold'>
+              <Link href='https://marededeudelassumpcio-badalona.educamos.com/'>
+                E-Mail: AMPA (Plataforma EDUCAMOS)
+              </Link>
             </p>
           </div>
         </div>
