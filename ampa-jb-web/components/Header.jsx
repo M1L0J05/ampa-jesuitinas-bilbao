0849b08e37f3f9f9139e492157c4817850c85d28
kkrotDev <moyo.gsxr@gmail.com>
2023-03-12 17:29:06 +0100

    Finalizado primer diseño de Header.

--- ampa-jb-web/components/Header.jsx
+++ ampa-jb-web/components/Header.jsx
@@ -1,34 +1,38 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
+import '../styles/header.css'
+
 export default function Header() {
   return (
-    <header className=' bg-gray-100 p-2'>
-      <div className='container grid grid-cols-1 m-auto lg:grid-cols-3 gap-1 p-2'>
-        <div className='p-2 flex justify-center lg:col-start-2 lg:col-end-3 lg:row-start-1'>
+    <header className='hearderBackground p-2 lg:p-4'>
+      <div className='grid grid-cols-1 m-auto lg:grid-cols-3 gap-1 p-2 '>
+        <div className='p-2 flex justify-center lg:col-start-2 lg:col-end-3 lg:row-start-1 '>
           <Image
             src='/assets/header/logo.png'
             alt='Logotipo de la AMPA'
-            width={150}
-            height={150}
+            width={300}
+            height={300}
           />
         </div>
-        <div className='text-blue-600 text-center text-3xl lg:col-start-1 lg:col-end-2 lg:row-start-1'>
-          Header - 1
+
+        <div className='relative h-32 p-2 lg:my-auto lg:col-start-1 lg:col-end-2 lg:row-start-1 lg:min-h-max rounded-3xl bg-white bg-opacity-50'>
+          <Image
+            src='/assets/header/somosjesuitinasLogo.png'
+            alt='Logotipo de la AMPA'
+            fill
+            style={{ objectFit: 'contain' }}
+          />
         </div>
-        <div className='lg:col-star-3 lg:col-end-4 lg:row-start-1 flex align-middle'>
-          <div className='p-2 lg:p-6 text-center font-sans lg:text-right m-auto '>
-            <h1 className='text-lg font-bold'>
+        <div className='flex align-middle rounded-3xl  bg-white bg-opacity-50 lg:col-star-3 lg:col-end-4 lg:row-start-1 lg:bg-opacity-0'>
+          <div className='p-2 lg:p-6 text-center font-sans lg:text-right m-auto rounded-3xl bg-white bg-opacity-0 lg:bg-opacity-50 '>
+            <h1 className='text-2xl font-bold'>
               AMPA "Jesuitinas Bilbao" Ikastetxea
             </h1>
-            <p className='text-naranja-900 text-xs'>
-              C/ Zabalbide 141, 48015 - Bilbao
-            </p>
-            <p className='text-naranja-900 text-xs'>
-              Tlfo: 94 445 42 00 (Horario escolar)
-            </p>
-            <p className='text-naranja-900 text-xs hover:text-morado-900 hover:font-bold'>
-              <Link href='https://marededeudelassumpcio-badalona.educamos.com/'>
+            <p className='tex-lg'>C/ Zabalbide 141, 48015 - Bilbao</p>
+            <p className='text-lg'>Tlfo: 94 445 42 00 (Horario escolar)</p>
+            <p className='text-lg hover:text-naranja-900 hover:font-bold'>
+              <Link href='https://inmaculada-hijasjesus-bilbao.educamos.com/'>
                 E-Mail: AMPA (Plataforma EDUCAMOS)
               </Link>
             </p>
