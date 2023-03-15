0095aee0a81a9558f029a9a9a296867b32d1dfb9
kkrotDev <moyo.gsxr@gmail.com>
2023-03-15 12:00:51 +0100

    Nuevo diseño del Header

--- ampa-jb-web/components/Header.jsx
+++ ampa-jb-web/components/Header.jsx
@@ -5,9 +5,9 @@ import '../styles/header.css'
 
 export default function Header() {
   return (
-    <header className='hearderBackground p-2 lg:p-4'>
-      <div className='grid grid-cols-1 m-auto lg:grid-cols-3 gap-1 p-2 '>
-        <div className='p-2 flex justify-center lg:col-start-2 lg:col-end-3 lg:row-start-1 '>
+    <header className='hearderBackground pt-4 lg:p-8'>
+      <div className='mx-32 grid grid-cols-1 lg:grid-cols-2'>
+        <div className='flex justify-center lg:justify-start'>
           <Image
             src='/assets/header/logo.png'
             alt='Logotipo de la AMPA'
@@ -15,28 +15,13 @@ export default function Header() {
             height={300}
           />
         </div>
-
-        <div className='relative h-32 p-2 lg:my-auto lg:col-start-1 lg:col-end-2 lg:row-start-1 lg:min-h-max rounded-3xl bg-white bg-opacity-50'>
+        <div className='flex justify-center my-auto lg:justify-end'>
           <Image
-            src='/assets/header/somosjesuitinasLogo.png'
-            alt='Logotipo de la AMPA'
-            fill
-            style={{ objectFit: 'contain' }}
+            src='/assets/header/contacto_ampa.png'
+            alt='Contacto de la AMPA'
+            width={500}
+            height={250}
           />
-        </div>
-        <div className='flex align-middle rounded-3xl  bg-white bg-opacity-50 lg:col-star-3 lg:col-end-4 lg:row-start-1 lg:bg-opacity-0'>
-          <div className='p-2 lg:p-6 text-center font-sans lg:text-right m-auto rounded-3xl bg-white bg-opacity-0 lg:bg-opacity-50 '>
-            <h1 className='text-2xl font-bold'>
-              AMPA "Jesuitinas Bilbao" Ikastetxea
-            </h1>
-            <p className='tex-lg'>C/ Zabalbide 141, 48015 - Bilbao</p>
-            <p className='text-lg'>Tlfo: 94 445 42 00 (Horario escolar)</p>
-            <p className='text-lg hover:text-naranja-900 hover:font-bold'>
-              <Link href='https://inmaculada-hijasjesus-bilbao.educamos.com/'>
-                E-Mail: AMPA (Plataforma EDUCAMOS)
-              </Link>
-            </p>
-          </div>
         </div>
       </div>
     </header>
